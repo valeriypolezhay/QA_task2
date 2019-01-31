c984756d0db0e6e840acaae8a001722edf46b3ac
Valerii Nativus <valeriypolezhay@gmail.com>
2019-01-31 13:11:55 +0200

    Add project files.

---
+++ Auto_dz2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace Auto_dz2
+{
+   
+
+
+    class FirstTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            
+            driver = new ChromeDriver();
+        }
+
+
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://accounts.google.com";
+        }
+
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+     
+   
+}
