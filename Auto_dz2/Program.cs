80867f484765f33972956c551ccbef6a3388bf6e
Valerii Nativus <valeriypolezhay@gmail.com>
2019-02-02 22:09:17 +0200

    done

--- Auto_dz2/Program.cs
+++ Auto_dz2/Program.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
+
 
 namespace Auto_dz2
 {
+    
 
-    class FirstTest
+    class NegativeTest
     {
         IWebDriver driver;
         IWebElement
@@ -39,7 +39,6 @@ namespace Auto_dz2
         [Test]
         public void test()
         {
-
 
             emailField = driver.FindElement(By.XPath("//*[@id='identifierId']"));
             emailField.SendKeys(wrongEmailLogin);
@@ -73,5 +72,69 @@ namespace Auto_dz2
 
     }
 
+    class PositiveTest
+    {
+        IWebDriver driver;
+        IWebElement
+            emailField,
+            emailNextButton,
+            passwordField,
+            passwordNextButton,
+            notificationWelcome;
 
+        
+        string correctEmailLogin = "valeoaeneus@gmail.com";
+        string correctEmailPassword = "totallynotsafepassword";
+
+        
+        string expectedText = "Welcome, ";
+
+
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://accounts.google.com";
+
+        }
+
+
+        [Test]
+        public void test()
+        {
+
+
+            emailField = driver.FindElement(By.XPath("//*[@id='identifierId']"));
+            emailField.SendKeys(correctEmailLogin);
+
+            emailNextButton = driver.FindElement(By.XPath("//*[@id='identifierNext']"));
+            emailNextButton.Click();
+
+            Thread.Sleep(3000);
+
+            passwordField = driver.FindElement(By.XPath("//*[@id='password']/div[1]/div/div[1]/input"));
+            passwordField.SendKeys(correctEmailPassword);
+
+            passwordNextButton = driver.FindElement(By.XPath("//*[@id='passwordNext']"));
+            passwordNextButton.Click();
+
+            Thread.Sleep(1000);
+
+            notificationWelcome = driver.FindElement(By.XPath("//*[@id='yDmH0d']/div[2]/c-wiz/div[1]/c-wiz/c-wiz/div/div[4]/div/div/header/h1"));
+
+            string resultText = notificationWelcome.Text;
+
+            Assert.IsTrue(resultText.Contains(expectedText), "no");
+
+        }
+
+
+        [TearDown]
+        public void closeBrowser()
+        {
+           driver.Close();
+        }
+
+    }
 }
